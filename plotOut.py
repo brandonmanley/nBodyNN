bdff2607ea9c59f679eafbc19207b965f16a9ea0
Brandon Manley <brandonmanley10@gmail.com>
2020-02-14 17:05:53 -0500

    update

--- plotOut.py
+++ plotOut.py
@@ -22,6 +22,7 @@ indexArray = np.unique((indexArray/ 10000).astype(int))
 print(indexArray)
 index = np.random.choice(indexArray)*10000
 # index = 20000
+# index = 20000
 # index = r.randint(1,len(in_df.index))
 # eventNum = in_df.iloc[index].eventID
 eventNum = index
--- test.jl
+++ test.jl
@@ -1,28 +1,19 @@
 using DifferentialEquations
 using Plots
 
-l = 1.0                    
-m = 1.0                         
-g = 9.81    
+m = 1.0                          
 ω = 1.0                     
 
-function mass_system!(ddu,du,u,p,t)
+function mass_system(du,u,p,t)
     # a(t) = (1/m) w^2 x 
-    ddu[1] = (1/m)*(ω^2)*u[1]
-    # du[1] = u[2]                                # θ'(t) = ω(t)
-    # du[2] = -3g/(2l)*sin(u[1]) + 3/(m*l^2)*p(t) # ω'(t) = -3g/(2l) sin θ(t) + 3/(ml^2)M(t)
+    (1/m)*(ω^2)*u[1]
 end
 
-# θ₀ = 0.01                         
-# ω₀ = 0.0  
-v0 = 0.0                      # initial velocity [m/s]
-u0 = 1.0                    # initial state vector
-tspan = (0.0,10.0)                  # time interval
+v0 = 0.0                     
+u0 = 1.0                  
+tspan = (0.0,10.0)               
 
-M = t->0.1sin(t)                    # external torque [Nm]
-
-# prob = ODEProblem(pendulum!,u₀,tspan,M)
-prob = SecondOrderODEProblem{isinplace}(mass_system!,v0,u0,tspan,callback=CallbackSet())
+prob = SecondOrderODEProblem(mass_system,v0,u0,tspan)
 sol = solve(prob)
 
 plotly()
