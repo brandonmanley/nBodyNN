3032cb318a1a731e211b714201f43ca5582c6c0e
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-03-13 18:12:02 -0400

    update

--- sort_div_events.py
+++ sort_div_events.py
@@ -6,43 +6,64 @@ import matplotlib.pyplot as plt
 import random as r 
 import glob
 import seaborn as sns
+from sklearn import metrics
+from collections import defaultdict
+from functools import partial
+from itertools import repeat
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, auc
+from keras.utils import to_categorical
+from keras import models
+from keras import layers
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Dense, Dropout, Activation
 sns.set()
 pd.options.mode.chained_assignment = None  # default='warn'
 
 simfilepath = "/mnt/c/Users/llave/Downloads/batch_brutus9_1.csv"
-df = pd.read_csv(simfilepath)
+workDir = '/mnt/c/users/llave/Documents/nBody'
+names = ["eventID","m1", "m2", "m3", 
+         "x1", "x2", "x3", "y1", "y2", "y3", "dx1", "dx2", "dx3", "dy1", "dy2", "dy3",
+         "tEnd",
+         "x1f", "x2f", "x3f", "y1f", "y2f", "y3f", "dx1f", "dx2f", "dx3f", "dy1f", "dy2f", "dy3f"]
+
+df = pd.read_csv(simfilepath,names=names)
 
 inputCols = ["m1", "m2", "m3", "x1", "x2", "x3", "y1", "y2", "y3", "dx1", "dx2", "dx3", "dy1", "dy2", "dy3"]
-df_new = pd.DataFrame()
+df_new = pd.DataFrame(columns = inputCols)
 n_div = 0
 div_col = []
-for i in range(280):
+
+n_div = 0
+
+for i in range(int(df.shape[0]/2560+1)):
+    
     index_i = i*10000
     index_f = i*10000+2560
-    event_pdf = df.loc[(df['eventID'] >= index_i) & (df['eventID'] <= (index_f))]
-    
-    tmp_df = df[inputCols]
-    df_new = pd.concat([tmp_df.iloc[[0]], df_new])
+    event_pdf = df[(df['eventID'] >= index_i) & (df['eventID'] <= (index_f))]
+
+    if(event_pdf.shape[0] <1): continue
+    tmp_df = event_pdf[inputCols]
+    df_new.loc[i]=tmp_df.iloc[0,:]
 
     if(event_pdf.shape[0]<2560):
         div_col.append("1")
+        n_div+=1
     else:
         div_col.append("0")
-
+print(len(div_col),df_new.shape)
 df_new["div"] = div_col
-
 print(n_div)
-print(df_div.shape)
-
-
 
 ######################################################################
 # nn stuffz
 ######################################################################
 
-data = df_new[:,15]
-X1 = data.values
-y1 = df_new["div"].values
+data = df_new.iloc[:,:15]
+X1 = data.to_numpy()
+y1 = df_new["div"].values 
 
 X_train,X_test,y_train,y_test = train_test_split(X1,y1, test_size=0.2, random_state=42)
 print(X_train.shape, y_train.shape)
@@ -51,30 +72,26 @@ print(X_test.shape,y_test.shape)
 X_train = X_train.astype('float64')
 X_test = X_test.astype('float64')
 
-print(y_train.shape,y_test.shape)
+y_train_cat = to_categorical(y_train)
+y_test_cat = to_categorical(y_test)
 
 #parameters 
-max_epochs = 100
+max_epochs = 50
+activation = 'tanh'
 optimizer = 'adam'
 batch_size = 20      
-loss_function = 'mse'   
 
-#early stopping
-patienceCount = 20
-callbacks = [EarlyStopping(monitor='val_loss', patience=patienceCount),
-             ModelCheckpoint(filepath=workDir+'/weights/best_model.h5', monitor='val_loss', save_best_only=True)]
-
+#
 network = models.Sequential()
-network.add(layers.Dense(64,activation='relu',input_dim=15))
-network.add(layers.Dense(1,activation='softmax'))
-network.compile(optimizer=optimizer,loss=loss_function,metrics=['accuracy'])
+network.add(layers.Dense(64,activation=activation,input_shape=(15,)))
+network.add(layers.Dense(2,activation='softmax'))
+network.compile(optimizer=optimizer,loss='categorical_crossentropy',metrics=['accuracy'])
 network.save_weights(workDir + '/weights/model_init.h5')
 
-history = network.fit(X_train,y_train,
+history = network.fit(X_train,y_train_cat,
                               epochs=max_epochs,
-                              callbacks = callbacks,
                               batch_size=batch_size,
-                              validation_data=(X_test,y_test),
+                              validation_data=(X_test,y_test_cat),
                               verbose = 1)
 
 training_vals_acc = history.history['accuracy']
