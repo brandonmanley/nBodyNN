620f3f86d9632a98fdcd9db6d25da22b42021862
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-04-04 18:42:33 -0400

    update

--- nn_kfold_train.py
+++ nn_kfold_train.py
@@ -10,14 +10,13 @@ from functools import partial
 from itertools import repeat
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 from sklearn.metrics import roc_curve, auc
 from keras.utils import to_categorical
 from keras import models
 from keras import layers
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Dense, Dropout, Activation
-import preputil as util
 
 workDir = "/users/PAS1585/llavez99/work/nbody/"
 dataDir = "/users/PAS1585/llavez99/data/nbody/"
@@ -35,10 +34,11 @@ def kfold_network(X, y, hidden_nodes,activation='relu',optimizer='adam'):
     numSplits = 0
     
     network = models.Sequential()
-    network.add(layers.Dense(hidden_nodes,activation='relu',input_dim=7))
-	for i in range(9):
-		network.add(layers.Dense(128,activation='relu'))
-    network.compile(optimizer=optimizer,loss='mean_squared_error',metrics=['accuracy'])
+    network.add(layers.Dense(hidden_nodes,activation='relu',input_dim=16))
+    for i in range(9):
+        network.add(layers.Dense(128,activation='relu'))
+    network.add(layers.Dense(12,activation='linear'))
+    network.compile(optimizer=optimizer,loss='mean_squared_logarithmic_error',metrics=['accuracy'])
     network.save_weights(workDir + '/weights/model_init.h5')
     
     #early stopping
@@ -46,6 +46,9 @@ def kfold_network(X, y, hidden_nodes,activation='relu',optimizer='adam'):
     callbacks = [EarlyStopping(monitor='val_loss', patience=patienceCount),
                  ModelCheckpoint(filepath=workDir+'/weights/best_model_split'+str(numSplits)+'_nhidden'+str(hidden_nodes)+'.h5', monitor='val_loss', save_best_only=True)]
 
+    #k-fold validation with 4 folds
+    kfolds = 4
+    
     training_vals_acc = 0
     training_vals_loss = 0
     valid_vals_acc = 0
@@ -58,9 +61,9 @@ def kfold_network(X, y, hidden_nodes,activation='relu',optimizer='adam'):
 
     #k-fold validation with 5 folds
     kfolds = 5
-    kf = KFold(n_splits=kfolds)
+    skf = KFold(n_splits=kfolds)
 
-    for train_index, val_index in kf.split(X, y):
+    for train_index, val_index in skf.split(X, y):
 
         print("Training on numSplit:",numSplits)
         numSplits += 1
@@ -73,9 +76,9 @@ def kfold_network(X, y, hidden_nodes,activation='relu',optimizer='adam'):
         history = network.fit(X_train,y_train,
                               callbacks = callbacks,
                               epochs=max_epochs,
-                              batch_size=128,
+                              batch_size=1000,
                               validation_data=(X_val,y_val), 
-                              verbose = 0)
+                              verbose = 1)
         
         network.save(workDir + '/weights/trained_model_split'+str(numSplits)+'_nhidden'+str(hidden_nodes)+'.h5')
 
@@ -119,35 +122,35 @@ def kfold_network(X, y, hidden_nodes,activation='relu',optimizer='adam'):
 
 
 
-
 workDir = "/mnt/c/Users/llave/Documents/nBody/"
 
 #Import data
-fname = workDir + "data/"
+fname = workDir + "data/batch_brutus10_4.csv"
 df = pd.read_csv(fname)
 with pd.option_context('mode.use_inf_as_null', True):
     df = df.dropna()
-
-dfShuffle = shuffle(df,random_state=42)
-i_col = ["m1","m2","m3","x1", "x2", "x3", "y1", "y2", "y3",
-	"dx1","dx2","dx3","dy1","dy2","dy3","tEnd"]
-o_col = ["x1tEnd", "x2tEnd", "x3tEnd", "y1tEnd", "y2tEnd", "y3tEnd",
-		"dx1tEnd", "dx2tEnd", "dx3tEnd", "dy1tEnd", "dy2tEnd", "dy3tEnd"]
-X = dfShuffle.as_matrix(columns=i_col)
-y = dfShuffle.as_matrix(columns=o_col)
-
-X = X.astype('float64')
-y = y.astype('float64')
 
 acc_list = []
 loss_list = []
 iterations_list = []
 nodes_list = [128]
 
+#dfShuffle = shuffle(df,random_state=42)
+i_col = ["m1","m2","m3","x1", "x2", "x3", "y1", "y2", "y3",
+    "dx1","dx2","dx3","dy1","dy2","dy3","tEnd"]
+o_col = ["x1tEnd", "x2tEnd", "x3tEnd", "y1tEnd", "y2tEnd", "y3tEnd",
+         "dx1tEnd", "dx2tEnd", "dx3tEnd", "dy1tEnd", "dy2tEnd", "dy3tEnd"]
+X = df.as_matrix(columns=i_col)
+y = df.as_matrix(columns=o_col)
+
+X = X.astype('float64')
+y = y.astype('float64')
+
 # Determine best number of hidden nodes for one charge, and apply it for other charges
 for nodes in nodes_list:
     
-    print("Nodes", nodes)
+    print("Training:")
+    print("Nodes:", nodes)
     
     #run train data through the network
     avg_acc,avg_loss,avg_iterations = kfold_network(X, y, nodes)
