eaa04d3ca088feffb1f5ffcd1d9b06e285d455e8
Luca Lavezzo <llavez99@pitzer-rw01.ten.osc.edu>
2020-05-05 14:05:48 -0400

    automatic simulation submission

--- dataGeneration/submit_train.py
+++ dataGeneration/submit_train.py
@@ -5,4 +5,4 @@ file_f = 10
 nBodies = 3
 for i in range(file_i, file_f+1):
     print("Submitting indata file",i,"running on",nBodies)
-    os.system("qsub -A PAS1585 run_brutus_sim.sh {0} {1}".format(i, nBodies))
+    os.system("qsub -A PAS1585 -v FILE={0},NBODIES={1} run_brutus_sim.sh".format(i, nBodies))
